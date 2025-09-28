811dbe690b6d232b37ead2bf4c8164e2631876cc
Luiz Gustavo <gustavo120wa@gmail.com>
2025-09-28 11:59:02 -0300

    add class barbeiro

---
+++ src/main/java/com/ufvjm/barbearia/model/Barbeiro.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ufvjm.barbearia.model;
+
+/**
+ *
+ * @author kirito
+ */
+public class Barbeiro extends Funcionario{
+    
+    public Barbeiro(String nome, String cpf) {
+        super(nome, cpf);
+    }
+    
+}

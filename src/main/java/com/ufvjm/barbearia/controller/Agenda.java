c9b712bc058773cb8bf893defd795fb1611be7ab
Luiz Gustavo <gustavo120wa@gmail.com>
2025-09-29 16:37:19 -0300

    Classe agenda

---
+++ src/main/java/com/ufvjm/barbearia/controller/Agenda.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ufvjm.barbearia.controller;
+
+import com.ufvjm.barbearia.model.Reserva;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ *
+ * @author kirito
+ */
+public class Agenda {
+    private List<Reserva> agendamentos = new ArrayList<>();
+    private Deque<Reserva> agendSecundario = new ArrayDeque<>();
+
+    public Agenda() {
+    }
+    
+    
+    
+    
+}
